d63458ce300b6bfe39120c52cce9f7ec056454eb
sproates <sproates@gmail.com>
2010-03-18 22:54:52 +0000

    added basic test harness

--- src/log.cpp
+++ src/log.cpp
@@ -5,7 +5,7 @@
 
 namespace peachy {
 
-  void Log::debug(const char *message) {
+  void Log::debug(const char * message) {
 #ifdef LOG_DEBUG_ENABLED
     std::cout << "DEBUG: " << message << std::endl;
 #endif
@@ -16,4 +16,18 @@ namespace peachy {
     std::cout << "DEBUG: " << message << std::endl;
 #endif
   }
+
+  void Log::info(const char * message) {
+#ifdef LOG_INFO_ENABLED
+    std::cout << "INFO: " << message << std::endl;
+#endif
+  }
+
+  void Log::info(std::string message) {
+#ifdef LOG_INFO_ENABLED
+    std::cout << "INFO : " << message << std::endl;
+#endif
+  }
+
 }
+

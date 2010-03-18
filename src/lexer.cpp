a7c5124da33aef56c56ad0f17d1baf9622d0980f
sproates <sproates@gmail.com>
2010-03-18 22:53:47 +0000

    added lexer skeleton

---
+++ src/lexer.cpp
@@ -0,0 +1,16 @@
+#include "lexer.h"
+
+#include "log.h"
+
+namespace peachy {
+
+  Lexer::Lexer() {
+    Log::debug("Lexer constructor");
+  }
+
+  Lexer::~Lexer() {
+    Log::debug("Lexer destructor");
+  }
+
+}
+

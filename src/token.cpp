07850a50f51c8dff56ec53ba8a30b03fd809486d
sproates <sproates@gmail.com>
2010-03-20 17:50:51 +0000

    added TokenType to Token

--- src/token.cpp
+++ src/token.cpp
@@ -4,9 +4,20 @@
 
 namespace peachy {
 
-  Token::Token(Log * logger) {
+  void Token::construct(Log * logger, TokenType tokenType) {
+    logger->debug("Token construct");
     this->logger = logger;
+    this->tokenType = tokenType;
+  }
+
+  Token::Token(Log * logger) {
     logger->debug("Token constructor");
+    construct(logger, TOKEN_EMPTY);
+  }
+
+  Token::Token(Log * logger, TokenType tokenType) {
+    logger->debug("Token constructor");
+    construct(logger, tokenType);
   }
 
   Token::~Token() {

fcdd8b13cb6fca6167613154c6af35d1d51742b1
sproates <sproates@gmail.com>
2010-03-20 17:50:51 +0000

    added TokenType to Token

--- src/headers/token.h
+++ src/headers/token.h
@@ -5,16 +5,34 @@ namespace peachy {
 
   class Log;
 
+  enum TokenType {
+
+    TOKEN_EMPTY,
+    TOKEN_EOF,
+    TOKEN_IDENTIFIER,
+    TOKEN_INTEGER,
+    TOKEN_KEYWORD,
+    TOKEN_NEWLINE,
+    TOKEN_OPERATOR,
+    TOKEN_STRING
+  };
+
   class Token {
 
     public:
 
       Token(Log * logger);
+      Token(Log * logger, TokenType tokenType);
       ~Token();
+
+      TokenType getTokenType();
 
     private:
 
       Log * logger;
+      TokenType tokenType;
+
+      void construct(Log * logger, TokenType tokenType);
 
       Token();
       Token(const Token & token);

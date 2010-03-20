98808cee9c2ca62aec3b3d06b80d167a6cd566d1
sproates <sproates@gmail.com>
2010-03-20 18:16:58 +0000

    fleshing out token

--- src/token.cpp
+++ src/token.cpp
@@ -1,27 +1,75 @@
 #include "token.h"
 
+#include <string>
+
 #include "log.h"
 
 namespace peachy {
 
-  void Token::construct(Log * logger, TokenType tokenType) {
-    logger->debug("Token construct");
+  void Token::construct(Log * logger, TokenType tokenType, std::string text) {
+    logger->debug("Token::construct()");
     this->logger = logger;
     this->tokenType = tokenType;
+    this->text = text;
   }
 
   Token::Token(Log * logger) {
     logger->debug("Token constructor");
-    construct(logger, TOKEN_EMPTY);
+    construct(logger, TOKEN_EMPTY, std::string(""));
   }
 
-  Token::Token(Log * logger, TokenType tokenType) {
+  Token::Token(Log * logger, TokenType tokenType, std::string text) {
     logger->debug("Token constructor");
-    construct(logger, tokenType);
+    construct(logger, tokenType, text);
   }
 
   Token::~Token() {
     logger->debug("Token destructor");
+  }
+
+  std::string Token::getText() const {
+    logger->debug("Token::getText()");
+    return text;
+  }
+
+  TokenType Token::getTokenType() const {
+    logger->debug("Token::getTokenType()");
+    return tokenType;
+  }
+
+  std::ostream & operator << (std::ostream & outputStream,
+                              const Token & token) {
+    switch(token.getTokenType()) {
+
+      case TOKEN_EMPTY:
+        outputStream << "empty";
+	break;
+      case TOKEN_EOF:
+        outputStream << "EOF";
+        break;
+      case TOKEN_IDENTIFIER:
+        outputStream << "identifier: " << token.getText();
+        break;
+      case TOKEN_INTEGER:
+        outputStream << "integer";
+        break;
+      case TOKEN_KEYWORD:
+        outputStream << "keyword: " << token.getText();
+        break;
+      case TOKEN_NEWLINE:
+        outputStream << "new line";
+        break;
+      case TOKEN_OPERATOR:
+        outputStream << "operator: " << token.getText();
+        break;
+      case TOKEN_STRING:
+        outputStream << "string: " << token.getText();
+        break;
+      default:
+        outputStream << "unknown token";
+	break;
+    }
+    return outputStream;
   }
 }
 

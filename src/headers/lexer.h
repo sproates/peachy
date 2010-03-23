e89c76fdbac0e124a0d7f6b62a5b9c797d23fece
sproates <sproates@gmail.com>
2010-03-23 14:52:03 +0000

    added LexerState

--- src/headers/lexer.h
+++ src/headers/lexer.h
@@ -1,6 +1,7 @@
 #ifndef PEACHY_LEXER_H
 #define PEACHY_LEXER_H
 
+#include "lexerstate.h"
 #include "log.h"
 #include "tokensource.h"
 
@@ -15,10 +16,15 @@ namespace peachy {
       Lexer(Log * logger, ScriptSource * scriptSource)
         : TokenSource(logger, scriptSource) {
         logger->debug("Lexer constructor");
+	state = LEXER_DEFAULT;
       }
       ~Lexer();
 
       Token * nextToken();
+
+    protected:
+
+      LexerState state;
 
     private:
 

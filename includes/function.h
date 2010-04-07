594c0f01d8512d08c32cead5b48f645604b8ab05
Ian Sproates <sproates@gmail.com>
2010-04-07 22:09:58 +0100

    Fleshing out Function class

--- includes/function.h
+++ includes/function.h
@@ -1,20 +1,29 @@
 #ifndef PEACHY_FUNCTION_H
 #define PEACHY_FUNCTION_H
 
+#include <deque>
+#include <string>
+
 namespace peachy {
 
+  class Expression;
   class Log;
 
   class Function {
 
     public:
 
-      Function(Log * logger);
+      Function(Log * logger, std::string name,
+        std::deque<Expression*> expressions);
       virtual ~Function();
+      std::string getName();
+      std::deque<Expression*> getExpressions();
 
     protected:
 
       Log * logger;
+      std::string name;
+      std::deque<Expression*> expressions;
   };
 }
 
--- src/function.cpp
+++ src/function.cpp
@@ -1,15 +1,32 @@
 #include "function.h"
 
+#include <deque>
+#include <string>
+
+#include "expression.h"
 #include "log.h"
 
 namespace peachy {
 
-  Function::Function(Log * logger) {
+  Function::Function(Log * logger, std::string name,
+    std::deque<Expression*> expressions) {
     logger->debug("Function constructor");
     this->logger = logger;
+    this->name = name;
+    this->expressions = expressions;
   }
 
   Function::~Function() {
     logger->debug("Function destructor");
+  }
+
+  std::string Function::getName() {
+    logger->debug("Function::getName()");
+    return name;
+  }
+
+  std::deque<Expression*> Function::getExpressions() {
+    logger->debug("Function::getExpressions()");
+    return expressions;
   }
 }

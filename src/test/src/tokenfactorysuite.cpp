fafe509ad8773b4e729f19443e3adf5896e41293
sproates <sproates@gmail.com>
2010-04-12 08:24:45 +0100

    TokenFactory unit tests

--- src/test/src/tokenfactorysuite.cpp
+++ src/test/src/tokenfactorysuite.cpp
@@ -69,6 +69,78 @@ namespace peachy {
       token = tokenFactory->createToken(logger, TOKEN_EOF);
       ASSERT_EQUALS(token->getTokenType() == TOKEN_EOF, "Token is TOKEN_EMPTY");
       ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(TOKEN_IDENTIFIER);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(logger, TOKEN_IDENTIFIER);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(TOKEN_IDENTIFIER, std::string("abc"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("abc")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(logger, TOKEN_IDENTIFIER, std::string("abc"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("abc")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(TOKEN_IDENTIFIER, std::string("apple_1"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("apple_1")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(logger, TOKEN_IDENTIFIER, std::string("apple_1"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_IDENTIFIER, "Token is TOKEN_IDENTIFIER");
+      ASSERT_EQUALS(token->getData().compare(std::string("apple_1")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(TOKEN_INTEGER);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(logger, TOKEN_INTEGER);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(TOKEN_INTEGER, std::string("1234"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("1234")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(logger, TOKEN_INTEGER, std::string("1234"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("1234")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(TOKEN_INTEGER, std::string("-52"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("-52")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(logger, TOKEN_INTEGER, std::string("-52"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_INTEGER, "Token is TOKEN_INTEGER");
+      ASSERT_EQUALS(token->getData().compare(std::string("-52")) == 0, "Token data is correct");
+
+      token = tokenFactory->createToken(TOKEN_KEYWORD);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(logger, TOKEN_KEYWORD);
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(TOKEN_KEYWORD, std::string("while"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("while")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(logger, TOKEN_KEYWORD, std::string("while"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("while")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(TOKEN_KEYWORD, std::string("for"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("for")) == 0, "Token data is empty");
+
+      token = tokenFactory->createToken(logger, TOKEN_KEYWORD, std::string("for"));
+      ASSERT_EQUALS(token->getTokenType() == TOKEN_KEYWORD, "Token is TOKEN_KEYWORD");
+      ASSERT_EQUALS(token->getData().compare(std::string("for")) == 0, "Token data is empty");
     }
   }
 }
